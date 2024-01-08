b3ef9b0f6cf7a975bc159c27d3974da72fe7fef7
k4ik <kaikleal1234@gmail.com>
2024-01-08 14:58:41 -0300

    feat: reset css

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-
+    <LoadingComponent/>
   </div>
 </template>
 
 <script>
+  import LoadingComponent from "./components/Loading/LoadingComponent.vue";
+
   export default {
-    
+    components: {
+      LoadingComponent
+    }
   }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 </style>
