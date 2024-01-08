05380fc277656fb0b8678b398f45f23884c45c3a
k4ik <kaikleal1234@gmail.com>
2024-01-08 14:54:24 -0300

    feat: loading component

---
+++ src/components/Loading/Loader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="loader"></div>
+</template>
+
+<script>
+    export default {
+        name: "Loader"
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "../../assets/scss/variables";
+
+    .loader {
+        animation: is-rotation 1s infinite;
+        border-radius: 50%;
+        width: 50px;
+        height: 50px;
+        border: 6px solid $grey;
+        border-top-color: $primary-color;
+    }
+
+    @keyframes is-rotation{
+        to {
+            transform: rotate(1turn);
+        }
+    }
+</style>
---
+++ src/components/Loading/LoadingComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <img src="../../assets/images/logo.png" alt="logo">
+        <Loader/>
+    </div>
+</template>
+
+<script>
+    import Loader from "./Loader.vue";
+
+    export default {
+        components: {
+            Loader
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+
+        img {
+            width: 45%;
+        }
+    }
+</style>
