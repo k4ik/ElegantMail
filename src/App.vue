3c30244357eaebba1b6d7d0d3d7273ec209ccabe
k4ik <kaikleal1234@gmail.com>
2024-02-03 19:36:23 -0300

    feat: reset css and import components

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,21 @@
 <template>
-  <div>
-    <LoadingComponent v-if="showLoading" />
-
-    <RegisterMessage v-if="showRegister" />
-  </div>
+  <Register />
 </template>
 
 <script>
-  import LoadingComponent from "./components/Loading/LoadingComponent.vue";
-  import RegisterMessage from "./components/RegisterMessage/RegisterMessage.vue";
+  import Register from './components/Register.vue'
 
   export default {
     components: {
-      LoadingComponent,
-      RegisterMessage,
-    },
-    data() {
-      return {
-        showLoading: true,
-        showRegister: false,
-      }
-    },
-    mounted() {
-      setTimeout(() => {
-        this.showLoading = false;
-        this.showRegister = true;
-      }, 3000);
+      Register
     }
   }
 </script>
-
 
 <style lang="scss">
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }  
+  }
 </style>
