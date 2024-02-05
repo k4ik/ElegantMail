4bda0d4af5121b9045bf0603b2f6aacb90df8715
k4ik <kaikleal1234@gmail.com>
2024-02-05 15:53:25 -0300

    feat: config routes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import RegisterView from './views/RegisterView.vue'
+import SuccessView from './views/SuccessView.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: RegisterView },
+  { path: '/success', component: SuccessView },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
