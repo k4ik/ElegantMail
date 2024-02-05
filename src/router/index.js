3661369825061fcc81c774c661a08eff8e039752
k4ik <kaikleal1234@gmail.com>
2024-02-05 16:12:19 -0300

    feat: config routes

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import RegisterView from '../views/RegisterView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/success',
+      name: 'success',
+      component: () => import('../views/SuccessView.vue')
+    }
+  ]
+})
+
+export default router
