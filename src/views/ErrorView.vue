1b1b47782093bdc9f64bb0c33e3ecd3c98c6e7f0
k4ik <kaikleal1234@gmail.com>
2024-02-06 17:47:31 -0300

    feat: create ErrorView

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,13 @@
+<template>
+    <Error />
+</template>
+
+<script>
+    import Error from '../components/Error.vue';
+
+    export default {
+        components: {
+            Error
+        }
+    }
+</script>
