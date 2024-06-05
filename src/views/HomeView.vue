7940b329fc5150ac89861e5d9086bb927cb4a058
k4ik <kaikleal1234@gmail.com>
2024-06-04 22:00:35 -0300

    feat: fetch api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,122 +1,159 @@
 <template>
     <main>
-        <form method="post" class="form" action="https://send-mail-api-r62r.vercel.app/send_mail.php">
+        <form class="form">
             <header class="header-form">
                 <label for="name" class="label-header">Você gostaria de se identificar?</label>
-                <input v-model="name" type="text" name="name" placeholder="Digite seu nome ou apelido" class="input-header">
+                <input v-model="name" type="text" name="name" placeholder="Digite seu nome ou apelido"
+                    class="input-header">
             </header>
 
             <div class="content-form">
                 <label for="email" class="label-form">Email</label>
-                <input v-model="email" type="email" name="email" placeholder="Digite o email dele ou dela" class="input-form">
+                <input v-model="email" type="email" name="email" placeholder="Digite o email dele ou dela"
+                    class="input-form">
                 <label for="message" class="label-form">Surpreenda</label>
-                <textarea v-model="message" name="message" class="input-form" placeholder="Solte o verbo para seu/sua amado(a)" rows="6"></textarea>
-                <button type="submit" class="submit-button">ENVIAR CORREIO</button>
+                <textarea v-model="message" name="message" class="input-form"
+                    placeholder="Solte o verbo para seu/sua amado(a)" rows="6"></textarea>
+                <button type="submit" class="submit-button" @click.prevent="submitData">ENVIAR CORREIO</button>
             </div>
         </form>
     </main>
 </template>
 
-<script></script>
+<script>
+export default {
+    methods: {
+        submitData() {
+            let formData = new FormData(document.querySelector('.form'));
+
+            fetch("https://submit-mail-api.000webhostapp.com/mailer", {
+                method: "POST",
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if(data.error) {
+                    this.$router.push("/error");
+                    return;
+                }
+
+                if(data.success) {
+                    this.$router.push("/success");
+                    return;
+                }
+            })
+            .catch(error => {
+                this.$router.push("/error");
+            })
+        }
+    }
+}
+</script>
 
 
 <style lang="scss" scoped>
-    @import '../assets/scss/variables';
+@import '../assets/scss/variables';
 
-    form {
+form {
+    width: 100%;
+
+    .header-form,
+    .content-form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .header-form {
+        padding: 0 10px;
+        background-color: $rosa-escuro;
         width: 100%;
+        height: 200px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        gap: 10px;
 
-        .header-form, .content-form {
-            display: flex;
-            flex-direction: column;
+        .label-header,
+        .input-header {
+            max-width: 350px;
+            width: 100%;
         }
 
-        .header-form {
-            padding: 0 10px;
-            background-color: $rosa-escuro;
-            width: 100%;
-            height: 200px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-            gap: 10px;
+        .label-header {
+            color: $branco;
+            font-size: 1.35em;
+            font-weight: bold;
 
-            .label-header, .input-header {
-                max-width: 350px;
-                width: 100%;
-            }
-
-            .label-header {
-                color: $branco;
-                font-size: 1.35em;
-                font-weight: bold;
-
-                @media screen and (max-width: 365px) {
-                    font-size: 20px;
-                }
-            }
-
-            .input-header {
-                border: none;
-                outline: none;
-                padding: 5px 10px;
-                border-radius: 12px;
-                &::placeholder {
-                    color: $cinza;
-                }
+            @media screen and (max-width: 365px) {
+                font-size: 20px;
             }
         }
 
-        .content-form {
-            padding: 0 10px;
-            margin-top: 80px;
-            width: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
+        .input-header {
+            border: none;
+            outline: none;
+            padding: 5px 10px;
+            border-radius: 12px;
 
-            .label-form, .input-form, .submit-button {
-                max-width: 350px;
-                width: 100%;
-            }
-
-            .label-form {
-                margin-bottom: 5px;
-                color: $cinza-escuro;
-                font-weight: 600;
-                font-size: 18px;
-            }
-
-            .input-form {
-                margin-bottom: 20px;
-                border: 1px solid $rosa-escuro;
-                padding: 5px 10px;
-                border-radius: 12px;
-                &:focus {
-                    outline: 1px solid $rosa-escuro;
-                }
-            }
-
-            .submit-button {
-                color: $branco;
-                background-color: $rosa-escuro;                
-                padding: 10px;
-                border: none;
-                border-radius: 12px;
-                font-weight: bold;
-                transition: .2s all ease-in-out;
-
-                &:hover {
-                    background-color: $rosa-claro;
-                }
-
-                &:focus {
-                    outline: 1px solid $rosa-escuro;
-                }
+            &::placeholder {
+                color: $cinza;
             }
         }
     }
+
+    .content-form {
+        padding: 0 10px;
+        margin-top: 80px;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        .label-form,
+        .input-form,
+        .submit-button {
+            max-width: 350px;
+            width: 100%;
+        }
+
+        .label-form {
+            margin-bottom: 5px;
+            color: $cinza-escuro;
+            font-weight: 600;
+            font-size: 18px;
+        }
+
+        .input-form {
+            margin-bottom: 20px;
+            border: 1px solid $rosa-escuro;
+            padding: 5px 10px;
+            border-radius: 12px;
+
+            &:focus {
+                outline: 1px solid $rosa-escuro;
+            }
+        }
+
+        .submit-button {
+            color: $branco;
+            background-color: $rosa-escuro;
+            padding: 10px;
+            border: none;
+            border-radius: 12px;
+            font-weight: bold;
+            transition: .2s all ease-in-out;
+
+            &:hover {
+                background-color: $rosa-claro;
+            }
+
+            &:focus {
+                outline: 1px solid $rosa-escuro;
+            }
+        }
+    }
+}
 </style>
